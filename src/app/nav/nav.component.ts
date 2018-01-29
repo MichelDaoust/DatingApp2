e878423dc486d0eb3d6c2cb51a4dccb53639f46b
Michel Daoust <michel.daoust@cgi.com>
2018-01-29 01:13:12 -0500

    error handling

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -19,7 +19,7 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(data => {
       console.log('logged in successfully');
     }, error => {
-      console.log('failed to login')
+      console.log(error)
     });
   }
   logout() {
@@ -33,5 +33,4 @@ export class NavComponent implements OnInit {
     return !!token;
   }
 
-  }
 }
